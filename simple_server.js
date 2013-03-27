6413ef7c614b91d3ce63fdf3118c0dcf53c4be73
anhtu <anhtu2687@gmail.com>
2013-03-27 08:06:43 +0700

    Refactored and Added mime-type

--- simple_server.js
+++ simple_server.js
@@ -4,36 +4,49 @@ var http = require('http'),
     fs   = require('fs');   // it's for file system
 
 var handleEventRequest = function (req, res) {
-  console.log("a request arrives");
-  console.log("request headers: " + req.headers); // object 
-  console.log("request url: " + req.url); // /
+    console.log("a request arrives");
+    console.log("request headers: " + req.headers); // object
+    console.log("request url: " + req.url); // /
 
-  /* request for ajax.html */
-  if (req.url.indexOf('ajax.html') > -1) {
-    console.log("request for ajax.html");
+    var filePath = req.url.slice(1);
+    console.log("filePath : " + filePath);
 
-    fs.readFile('ajax.html', function (err, html) {
-    
-      if (err) { console.log(err); return ; }
+    /* once we got the file path we can start to read file */
+    fs.readFile(filePath, function (err, html) {
 
-      res.writeHead(200, { "Content-Type": "text/html" }); // 200 means OK - content is there
-      res.write(html);
-      res.end();
+        if (err) { console.log(err); return ; }
+
+        var mimeType = {
+            "html"  : "text/html",
+            "css"   : "text/css",
+            "js"    : "text/javascript",
+            "xml"   : "text/xml",
+
+            /* image type */
+            "png"   : "image/png",
+            "gif"   : "image/gif",
+            "jpeg"  : "image/jpeg",
+
+            /* application */
+            "json"  : "application/json",
+            "default" : "text/html"
+        };
+
+        /* detect the file extension */
+        var extension = "default",
+            dot       = filePath.lastIndexOf('.');
+        if (dot > -1) {
+            extension = filePath.substr(dot + 1);
+        }
+
+        console.log("extension : " + extension);
+        console.log(" content - type : " + mimeType[extension]);
+        /* now we have to customize our header */
+        res.writeHead(200, { "Content-Type": mimeType[extension] }); // 200 means OK - content is there
+        res.write(html);
+        res.end();
     });
-  
-  }
-  else if (req.url.indexOf('index.html') > -1) {
-    console.log("request for index.html");
 
-    fs.readFile('index.html', function (err, html) {
-    
-      if (err) { console.log(err); return ; }
-
-      res.writeHead(200, { "Content-Type": "text/html" }); // 200 means OK - content is there
-      res.write(html);
-      res.end();
-    });
-  }
 };
 
 var server = http.createServer( handleEventRequest );
